2e6947620303a4a8c16871313fbfc80788536bbe
Brandon Rennolds <rennoldsb@gmail.com>
2020-08-03 19:07:53 +0000

    new_contact_path > contact_path controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -12,10 +12,10 @@ class ContactsController < ApplicationController
             body = params[:contact][:comments]
             ContactMailer.contact_email(name, email, body).deliver
             flash[:success] = "Message sent."
-            redirect_to new_contact_path
+            redirect_to contact_path
         else
             flash[:danger] = @contact.errors.full_messages.join(", ")
-            redirect_to new_contact_path
+            redirect_to contact_path
         end
     end
     
